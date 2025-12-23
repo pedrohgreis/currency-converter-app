a16fb4b112843252a710175c8873d80c2218c560
Pedro <pedrohgdreis@gmail.com>
2025-12-22 21:20:07 -0400

    feat: adding delete-currency usecase

--- backend/src/Services/useCases/deleteCurrencyUseCase.ts
+++ backend/src/Services/useCases/deleteCurrencyUseCase.ts
@@ -1,19 +1,20 @@
 import type { Currency } from "@/generated/prisma";
 import type { CurrencyRepository } from "@/repositories/prisma/currencyRepository";
+import { CurrencyNotFound } from "../Error/currency-not-found";
 import { CurrencyNotDeleted } from "../Error/currency-not-deleted";
 
-interface GetUserProfileUseCaseRequest{
+interface GetCurrencyUseCaseRequest{
     currencyId: string;
 }
  
-interface GetUserProfileUseCaseResponse{
+interface GetCurrencyUseCaseResponse{
     currency: Currency,
 }
 
 export class DeleteCurrencyUseCase{
     constructor(private readonly currencyRepository: CurrencyRepository){}
 
-    async delete({currencyId}: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse>{
+    async delete({currencyId}: GetCurrencyUseCaseRequest): Promise<GetCurrencyUseCaseResponse>{
         const currency = await this.currencyRepository.deleteCurrency(currencyId)
 
         if(!currency) throw new CurrencyNotDeleted() 
